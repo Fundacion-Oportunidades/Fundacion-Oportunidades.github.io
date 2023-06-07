2413880517a4090d2192e36cd79db93283eb6a04
AlannV <97909677+AlannV@users.noreply.github.com>
2023-06-07 08:22:55 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/site",
+  base: "/",
   site: "https://fundacion-oportunidades.github.io/site/",
   trailingSlash: "never",
 });
