d5dc241cfd0811e16fd8a840673f7a20f9198cdd
Sebastian Amin Fariña <amiin74@gmail.com>
2023-09-28 09:12:17 -0300

    Create folder in cloud, (FIREBASE) and change paths

--- src/data/sliderHome.ts
+++ src/data/sliderHome.ts
@@ -3,19 +3,19 @@ import type { ImageList } from "../types/types";
 const sliderList: ImageList[] = [
   {
     name: "Imposible 1",
-    imageUrl: "./assets/Recursos/JPG/3.png"
+    imageUrl: "https://firebasestorage.googleapis.com/v0/b/fundacion-oportunidades.appspot.com/o/Assets%2FRecursos%2FJPG%2F3.png?alt=media&token=8133b843-1451-4ecb-afd0-53e18668dda3&_gl=1*1jq4avb*_ga*MzU5MzA0NDAuMTY5NTkwMTE2OQ..*_ga_CW55HF8NVT*MTY5NTkwMTE3MC4xLjEuMTY5NTkwMjg2OC41NS4wLjA."
   },
   {
     name: "Imposible 2",
-    imageUrl: "./assets/Recursos/JPG/lapiz imposible.png"
+    imageUrl: "https://firebasestorage.googleapis.com/v0/b/fundacion-oportunidades.appspot.com/o/Assets%2FRecursos%2FJPG%2Flapiz%20imposible.png?alt=media&token=a2410f5c-ea18-4fe4-b89e-5cbe5a8e218b&_gl=1*iw0cnp*_ga*MzU5MzA0NDAuMTY5NTkwMTE2OQ..*_ga_CW55HF8NVT*MTY5NTkwMTE3MC4xLjEuMTY5NTkwMjg4OC4zNS4wLjA."
   },
   {
     name: "Imposible 3",
-    imageUrl: "./assets/Recursos/JPG/pintor imposible.png"
+    imageUrl: "https://firebasestorage.googleapis.com/v0/b/fundacion-oportunidades.appspot.com/o/Assets%2FRecursos%2FJPG%2Fpintor%20imposible.png?alt=media&token=8561c137-d577-453f-8db6-c788137a9a47&_gl=1*832ucw*_ga*MzU5MzA0NDAuMTY5NTkwMTE2OQ..*_ga_CW55HF8NVT*MTY5NTkwMTE3MC4xLjEuMTY5NTkwMjkwNy4xNi4wLjA."
   },
   {
     name: "Imposible 4",
-    imageUrl: "./assets/Recursos/JPG/pizarron imposible.png"
+    imageUrl: "https://firebasestorage.googleapis.com/v0/b/fundacion-oportunidades.appspot.com/o/Assets%2FRecursos%2FJPG%2Fpizarron%20imposible.png?alt=media&token=7cb8537b-e032-4e69-8d71-66b46f14adac&_gl=1*6rcgrj*_ga*MzU5MzA0NDAuMTY5NTkwMTE2OQ..*_ga_CW55HF8NVT*MTY5NTkwMTE3MC4xLjEuMTY5NTkwMjkyOC42MC4wLjA."
   },
 
 ];
--- src/data/socialMedia.ts
+++ src/data/socialMedia.ts
@@ -12,9 +12,9 @@ const socialMediaItems: SocialMediaTypes[] = [
   {
     url: "https://twitter.com/Oportunidades_F?t=ixuRbRVmYsQLl5Ng3OC--A&s=08",
     imageUrlInactive:
-      "https://res.cloudinary.com/dsblswewr/image/upload/v1685355461/twitter_gris_bcfe4m.png",
+      "https://firebasestorage.googleapis.com/v0/b/fundacion-oportunidades.appspot.com/o/Assets%2FRecursos%2FPNG%2Ftwitter-x-gris.png?alt=media&token=2269e5a0-f846-426f-81e1-71622a06d49d&_gl=1*10zgqxz*_ga*MzU5MzA0NDAuMTY5NTkwMTE2OQ..*_ga_CW55HF8NVT*MTY5NTkwMTE3MC4xLjEuMTY5NTkwMjcwMC42MC4wLjA.",
     imageUrlActive:
-      "./assets/Recursos/PNG/twitter-x.png",
+      "https://firebasestorage.googleapis.com/v0/b/fundacion-oportunidades.appspot.com/o/Assets%2FRecursos%2FPNG%2Ftwitter-x.png?alt=media&token=8b2a2845-a20d-4688-a2c5-6cf8c665278b&_gl=1*126zlpt*_ga*MzU5MzA0NDAuMTY5NTkwMTE2OQ..*_ga_CW55HF8NVT*MTY5NTkwMTE3MC4xLjEuMTY5NTkwMjc2MC42MC4wLjA.",
     imageAlt: "twitter",
   },
   {
