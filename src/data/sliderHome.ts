d6029c2cb5cbae79c0188cefa97f4242c2441482
Amin-farina <amiin74@gmail.com>
2023-10-12 11:07:16 -0300

    Ultimos cambios, chequeados despues de la call

--- src/data/sliderHome.ts
+++ src/data/sliderHome.ts
@@ -9,14 +9,14 @@ const sliderList: ImageList[] = [
     name: "Imposible 2",
     imageUrl: "https://firebasestorage.googleapis.com/v0/b/fundacion-oportunidades.appspot.com/o/Assets%2FRecursos%2FJPG%2Flapiz%20imposible.png?alt=media&token=a2410f5c-ea18-4fe4-b89e-5cbe5a8e218b&_gl=1*iw0cnp*_ga*MzU5MzA0NDAuMTY5NTkwMTE2OQ..*_ga_CW55HF8NVT*MTY5NTkwMTE3MC4xLjEuMTY5NTkwMjg4OC4zNS4wLjA."
   },
-  {
-    name: "Imposible 3",
-    imageUrl: "https://firebasestorage.googleapis.com/v0/b/fundacion-oportunidades.appspot.com/o/Assets%2FRecursos%2FJPG%2Fpintor%20imposible.png?alt=media&token=8561c137-d577-453f-8db6-c788137a9a47&_gl=1*832ucw*_ga*MzU5MzA0NDAuMTY5NTkwMTE2OQ..*_ga_CW55HF8NVT*MTY5NTkwMTE3MC4xLjEuMTY5NTkwMjkwNy4xNi4wLjA."
-  },
-  {
-    name: "Imposible 4",
-    imageUrl: "https://firebasestorage.googleapis.com/v0/b/fundacion-oportunidades.appspot.com/o/Assets%2FRecursos%2FJPG%2Fpizarron%20imposible.png?alt=media&token=7cb8537b-e032-4e69-8d71-66b46f14adac&_gl=1*6rcgrj*_ga*MzU5MzA0NDAuMTY5NTkwMTE2OQ..*_ga_CW55HF8NVT*MTY5NTkwMTE3MC4xLjEuMTY5NTkwMjkyOC42MC4wLjA."
-  },
+  // {
+  //   name: "Imposible 3",
+  //   imageUrl: "https://firebasestorage.googleapis.com/v0/b/fundacion-oportunidades.appspot.com/o/Assets%2FRecursos%2FJPG%2Fpintor%20imposible.png?alt=media&token=8561c137-d577-453f-8db6-c788137a9a47&_gl=1*832ucw*_ga*MzU5MzA0NDAuMTY5NTkwMTE2OQ..*_ga_CW55HF8NVT*MTY5NTkwMTE3MC4xLjEuMTY5NTkwMjkwNy4xNi4wLjA."
+  // },
+  // {
+  //   name: "Imposible 4",
+  //   imageUrl: "https://firebasestorage.googleapis.com/v0/b/fundacion-oportunidades.appspot.com/o/Assets%2FRecursos%2FJPG%2Fpizarron%20imposible.png?alt=media&token=7cb8537b-e032-4e69-8d71-66b46f14adac&_gl=1*6rcgrj*_ga*MzU5MzA0NDAuMTY5NTkwMTE2OQ..*_ga_CW55HF8NVT*MTY5NTkwMTE3MC4xLjEuMTY5NTkwMjkyOC42MC4wLjA."
+  // },
 
 ];
 
--- src/data/universities.ts
+++ src/data/universities.ts
@@ -3,22 +3,22 @@ import type { Universities } from "../types/types";
 const universities: Universities[] = [
   {
     imageUrl:
-      "https://res.cloudinary.com/dsblswewr/image/upload/v1686690015/dcu_logo_i2rux9.png",
-    imageAlt: "Logo DCU",
-    url: "https://www.dcu.ie/",
+      "https://firebasestorage.googleapis.com/v0/b/fundacion-oportunidades.appspot.com/o/Assets%2FLogos%20universidades%2FUAI-03.png?alt=media&token=0218a829-53b6-48fd-a14d-1a4ef1149e78",
+    imageAlt: "Universidad Abierta Interamericana",
+    url: "https://uai.edu.ar/",
   },
-  {
-    imageUrl:
-      "https://res.cloudinary.com/dsblswewr/image/upload/v1686690015/limerick_logo_kru7tp.png",
-    imageAlt: "Logo Limerick University",
-    url: "https://www.ul.ie/",
-  },
-  {
-    imageUrl:
-      "https://res.cloudinary.com/dsblswewr/image/upload/v1686690016/ucd_logo_rvflr9.png",
-    imageAlt: "Logo UCD",
-    url: "https://www.ucd.ie/",
-  },
+  // {
+  //   imageUrl:
+  //     "https://res.cloudinary.com/dsblswewr/image/upload/v1686690015/limerick_logo_kru7tp.png",
+  //   imageAlt: "Logo Limerick University",
+  //   url: "https://www.ul.ie/",
+  // },
+  // {
+  //   imageUrl:
+  //     "https://res.cloudinary.com/dsblswewr/image/upload/v1686690016/ucd_logo_rvflr9.png",
+  //   imageAlt: "Logo UCD",
+  //   url: "https://www.ucd.ie/",
+  // },
 ];
 
 export default universities;
