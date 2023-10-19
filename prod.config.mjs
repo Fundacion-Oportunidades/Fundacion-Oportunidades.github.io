624ba0713f75c1cc98d60ac512dc8cd406566314
Emanuel Calvo <3manuek@gmail.com>
2023-10-20 01:23:42 +0200

    fix: temporal workaround for the environment build
    Signed-off-by: Emanuel Calvo <3manuek@gmail.com>

--- prod.config.mjs
+++ prod.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-const isDevelopment = process.env.NODE_ENV === "development";
+// const isDevelopment = process.env.NODE_ENV === "development";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/site/",
-  site: "https://fundacion-oportunidades.github.io/site/",
+  base: "/",
+  site: "https://oportunidades.org.ar/",
   trailingSlash: "never",
   routes: {
-    "/site/": "./src/pages/index.astro",
-    "/site/about_us/": "./src/pages/about_us.astro",
+   "/": "./src/pages/index.astro",
+   "/about_us/": "./src/pages/about_us.astro",
   },
   server: {
     port: 3050,
