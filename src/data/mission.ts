af0e99a82227f36ef5491adb827e0f9fe3987ea5
Amin-farina <amiin74@gmail.com>
2023-10-20 11:24:35 -0300

    Change path all images

--- src/data/mission.ts
+++ src/data/mission.ts
@@ -8,13 +8,13 @@ const missionList: Mission[] = [
       'Así, pretendemos transformarnos en el instrumento para potenciar el futuro de personas que, de otra manera, su destino sería muy distinto.',
     ],
     imageUrl:
-      'https://firebasestorage.googleapis.com/v0/b/fundacion-oportunidades.appspot.com/o/Assets%2FRecursos%2FPNG%2Fhombre%20en%20contenedor.png?alt=media&token=c1045c42-4f63-48ea-9f10-228c17b4e14c',
+      'public/assets/Recursos/PNG/hombre en contenedor.png',
     position: 
       '1',
   },
   {
     title: 'Visión',
-    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/fundacion-oportunidades.appspot.com/o/Assets%2FRecursos%2FJPG%2Fescalera%20diagonal.jpg?alt=media&token=014b26eb-ab14-4c28-92d8-e3d49de4cc41',
+    imageUrl: 'public/assets/Recursos/PNG/escalera diagonal.png',
     paragraphs: [
       'Pretendemos ser reconocida nacionalmente como la institución, por excelencia, dedicada a crear oportunidades para personas de bajos recursos.',
     ],
@@ -22,7 +22,7 @@ const missionList: Mission[] = [
   },
   {
     title: 'Objetivo',
-    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/fundacion-oportunidades.appspot.com/o/Assets%2FRecursos%2FPNG%2Feducation-illustration.png?alt=media&token=04bbd51e-c1ce-4afc-aa6b-49f7df406b70',
+    imageUrl: 'public/assets/Recursos/PNG/education-illustration.png',
     paragraphs: [
       'La Fundación Oportunidades, en base a los objetivos de ayudar al crecimiento personal y profesional de personas de bajos recursos, tiene como principal campo de acción el otorgamiento de becas de estudio y estipendios mensuales, a alumnos que cuentan con buen rendimiento académico.',
       'La idea de trabajo es muy simple: otorgar becas de estudio a personas de bajos recursos y con buen rendimiento académico, para que puedan estudiar en universidades privadas del país. Es decir, la idea es darle oportunidades a aquellas personas que, de otra manera, no las tendrían.'
--- src/data/socialMedia.ts
+++ src/data/socialMedia.ts
@@ -4,33 +4,33 @@ const socialMediaItems: SocialMediaTypes[] = [
   {
     url: "https://www.facebook.com",
     imageUrlInactive:
-      "https://res.cloudinary.com/dsblswewr/image/upload/v1685355460/face_gris_ifzgrs.png",
+      "public/assets/RedesSociales/face_gris.png",
     imageUrlActive:
-      "https://res.cloudinary.com/dsblswewr/image/upload/v1685355460/face_azul_g84iel.png",
+      "public/assets/RedesSociales/face_azul.png",
     imageAlt: "facebook",
   },
   {
     url: "https://twitter.com/Oportunidades_F?t=ixuRbRVmYsQLl5Ng3OC--A&s=08",
     imageUrlInactive:
-      "https://firebasestorage.googleapis.com/v0/b/fundacion-oportunidades.appspot.com/o/Assets%2FRecursos%2FPNG%2Ftwitter-x-gris.png?alt=media&token=2269e5a0-f846-426f-81e1-71622a06d49d&_gl=1*10zgqxz*_ga*MzU5MzA0NDAuMTY5NTkwMTE2OQ..*_ga_CW55HF8NVT*MTY5NTkwMTE3MC4xLjEuMTY5NTkwMjcwMC42MC4wLjA.",
+      "public/assets/RedesSociales/twitter-x-gris.png",
     imageUrlActive:
-      "https://firebasestorage.googleapis.com/v0/b/fundacion-oportunidades.appspot.com/o/Assets%2FRecursos%2FPNG%2Ftwitter-x.png?alt=media&token=8b2a2845-a20d-4688-a2c5-6cf8c665278b&_gl=1*126zlpt*_ga*MzU5MzA0NDAuMTY5NTkwMTE2OQ..*_ga_CW55HF8NVT*MTY5NTkwMTE3MC4xLjEuMTY5NTkwMjc2MC42MC4wLjA.",
+      "public/assets/RedesSociales/twitter-x.png",
     imageAlt: "twitter",
   },
   {
     url: "https://www.linkedin.com/company/fundacion-oportunidades/",
     imageUrlInactive:
-      "https://res.cloudinary.com/dsblswewr/image/upload/v1685355461/ln_gris_k1nap3.png",
+      "public/assets/RedesSociales/ln_gris.png",
     imageUrlActive:
-      "https://res.cloudinary.com/dsblswewr/image/upload/v1685355461/ln_azul_dqnmwn.png",
+      "public/assets/RedesSociales/ln_azul.png",
     imageAlt: "linkedin",
   },
   {
     url: "https://instagram.com/fundacion.oportunidades?igshid=MzRlODBiNWFlZA==",
     imageUrlInactive:
-      "https://res.cloudinary.com/dsblswewr/image/upload/v1685355460/insta_gris_xcva8o.png",
+      "public/assets/RedesSociales/insta_gris.png",
     imageUrlActive:
-      "https://res.cloudinary.com/dsblswewr/image/upload/v1685355460/insta_color_mobgba.png",
+      "public/assets/RedesSociales/insta_color.png",
     imageAlt: "instagram",
   },
 ];
