0722fc287ab542676b397c6cbb21577376879f3a
Amin-farina <amiin74@gmail.com>
2023-10-23 18:18:47 -0300

    Change urls in companies.ts & universities.ts

--- src/data/companies.ts
+++ src/data/companies.ts
@@ -3,19 +3,19 @@ import type { Companies } from "../types/types";
 const companies: Companies[] = [
   {
     imageUrl:
-      "https://res.cloudinary.com/dsblswewr/image/upload/v1686690625/nintendo_thw6sv.png",
+      "/assets/nintendo-logo.png",
     imageAlt: "Nintendo Logo",
     url: "https://www.nintendo.com/",
   },
   {
     imageUrl:
-      "https://res.cloudinary.com/dsblswewr/image/upload/v1686690625/samsung_cabw4t.png",
+      "/assets/microsoft.png",
     imageAlt: "Samsung Logo",
     url: "https://www.samsung.com/ar/",
   },
   {
     imageUrl:
-      "https://res.cloudinary.com/dsblswewr/image/upload/v1686690624/microsoft_qp7vgq.png",
+      "/assets/microsoft.png",
     imageAlt: "Microsoft Logo",
     url: "https://www.microsoft.com/es-ar/",
   },
