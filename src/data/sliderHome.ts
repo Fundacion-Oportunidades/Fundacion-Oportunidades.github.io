79c385481c9d2bd10508865c5fccd83bb8ed7007
Amin-farina <amiin74@gmail.com>
2023-11-01 11:36:44 -0300

    Fix font safari

--- src/data/sliderHome.ts
+++ src/data/sliderHome.ts
@@ -1,14 +1,14 @@
 import type { ImageList } from "../types/types";
 
 const sliderList: ImageList[] = [
-  {
-    name: "Imposible 1",
-    imageUrl: "/assets/Recursos/JPG/3.png"
-  },
-  {
-    name: "Imposible 2",
-    imageUrl: "/assets/Recursos/JPG/lapiz imposible.png"
-  },
+  // {
+  //   name: "Imposible 1",
+  //   imageUrl: "/assets/Recursos/JPG/3.png"
+  // },
+   {
+     name: "Imposible 2",
+     imageUrl: "/assets/Recursos/JPG/lapiz imposible.png"
+   },
   // {
   //   name: "Imposible 3",
   //   imageUrl: "https://firebasestorage.googleapis.com/v0/b/fundacion-oportunidades.appspot.com/o//assets%2FRecursos%2FJPG%2Fpintor%20imposible.png?alt=media&token=8561c137-d577-453f-8db6-c788137a9a47&_gl=1*832ucw*_ga*MzU5MzA0NDAuMTY5NTkwMTE2OQ..*_ga_CW55HF8NVT*MTY5NTkwMTE3MC4xLjEuMTY5NTkwMjkwNy4xNi4wLjA."
